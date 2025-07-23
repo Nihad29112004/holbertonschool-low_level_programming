f8b08db330be15012ca0eb8849ca30b3b86ff043
Nihad ALiyev <lnihad257@gmail.com>
2025-07-23 18:48:54 +0000

    Task 10: Implement add function to sum two integers

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,15 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * add - adds two integers and returns the result
- * @a: first integer
- * @b: second integer
+ * main - check the code
  *
- * Return: sum of a and b
+ * Return: Always 0.
  */
-int add(int a, int b)
+int main(void)
 {
-    return (a + b);
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
 }
-
-
