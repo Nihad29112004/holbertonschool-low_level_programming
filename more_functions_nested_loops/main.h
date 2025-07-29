43e444ad0dd6755d36cabbc9c108cb4c01f966d2
Nihad Aliyev <lnihad257@gmail.com>
2025-07-29 11:14:56 +0000

    I believe in numbers and signs

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c); /* Dəyişmir, prototip eyni qalır */
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
 
