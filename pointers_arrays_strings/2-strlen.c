b79ca85e5a767000b23d42fb5f7997ab58dbcafa
Nihad Aliyev <lnihad257@gmail.com>
2025-07-31 10:18:49 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+size_t _strlen(const char *s)
+{
+size_t i = 0;
+while (s[i] != '\0')
+i++;
+return i;
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+size_t _strlen(const char *s);
 
 #endif
