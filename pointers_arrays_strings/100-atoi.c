4c49a66f1178258cfec4673d14b16596de60d324
Nihad Aliyev <lnihad257@gmail.com>
2025-07-31 14:39:32 +0000

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,6 +15,7 @@ int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
+
 do {
 if (*s == '-')
 sign *= -1;
@@ -23,5 +24,7 @@ num = (num * 10) + (*s - '0');
 else if (num > 0)
 break;
 } while (*s++);
+
 return (num * sign);
 }
+
