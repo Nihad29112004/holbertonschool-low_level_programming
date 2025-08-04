cdc5da53587c7d509615b6825c8dacceeff72403
Nihad Aliyev <lnihad257@gmail.com>
2025-08-04 11:12:58 +0000

    2-strchr

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ int _atoi(char *s);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, const char *src, unsigned int n);
-
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
 
