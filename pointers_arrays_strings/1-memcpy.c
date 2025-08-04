f42c8bea1f6bdd5c54361468278e9bda63bb5faa
Nihad Aliyev <lnihad257@gmail.com>
2025-08-04 11:05:20 +0000

    1-memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _memcpy - Custom function to fill memory with a constant byte.
+ * @s: Pointer to the memory area.
+ * @b: The byte to fill with.
+ * @n: Number of bytes to fill.
+ *
+ * Return: Pointer to memory area s.
+ */
 
 char *_memcpy(char *dest, const char *src, unsigned int n)
 {
